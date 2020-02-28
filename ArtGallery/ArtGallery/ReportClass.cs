1f891cfb9110145d7411d58619b11e19cb92d2a2
NEFUstudent <study.svfu@gmail.com>
2020-02-28 13:03:31 +0900

    Added ReportClass

--- ArtGallery/ArtGallery/ReportClass.cs
+++ ArtGallery/ArtGallery/ReportClass.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ArtGallery
 {
     class ReportClass
     {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime ReportDate { get; set; }
+        public int EmployeeId { get; set; }
+        [ForeignKey("EmplyeeId")]
+        public virtual Employee Employee { get; set; }
+
     }
 }
