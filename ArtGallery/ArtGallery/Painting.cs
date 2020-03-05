d83b432c6396abede1e3a50ce97d9ebf94701f80
aitalyy <aitalyy@gmail.com>
2020-03-05 15:28:58 +0900

    Almost there

--- ArtGallery/ArtGallery/Painting.cs
+++ ArtGallery/ArtGallery/Painting.cs
@@ -9,8 +9,6 @@ namespace ArtGallery
 {
     enum state { Otlichnoe, TrebuetRestavracii, Util }
     enum status { NaRestavracii, NaExposicii, NaSklade }
-    enum Genre {a,b,c}
-    enum PaintingTehnique {a,b,c }
     class Painting
     {
         [Key]
@@ -18,14 +16,19 @@ namespace ArtGallery
         public string Name { get; set; }
         public int ArtistId { get; set; }
         [ForeignKey("ArtistId")]
-        public virtual Artist Artist { get; set; }      
-        public Genre Genre { get; set; }
-        public PaintingTehnique PaintingTehnique { get; set; }
+        public virtual Artist Artist { get; set; }
+        public int GenreId { get; set; }
+        [ForeignKey("GenreId")]
+        public virtual Genre Genre { get; set; }
+        public int PaintingTehniqueId { get; set; }
+        [ForeignKey("PaintingTehniqueId")]
+        public virtual PaintingTehnique PaintingTehnique { get; set; }
         public DateTime DateOfPainting { get; set; }
         public double Price { get; set; }
         public state State { get; set; }
         public status Status { get; set; }
         public ICollection<Report> Reports { get; set; }
+
 
     }
 }
---
+++ ArtGallery/ArtGallery/PaintingTehnique.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ArtGallery
+{
+    class PaintingTehnique
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
