0f3bb110a149ee8c651df7c35099eb4770ff1b22
NEFUstudent <study.svfu@gmail.com>
2020-03-13 13:15:05 +0900

    Changed ExpoPlan form

--- ArtGallery/ArtGallery/Form2.Designer.cs
+++ ArtGallery/ArtGallery/Form2.Designer.cs
@@ -101,7 +101,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form2";
-            this.Text = "Form2";
+            this.Text = "Добавление экспозиции";
             this.ResumeLayout(false);
             this.PerformLayout();
 
