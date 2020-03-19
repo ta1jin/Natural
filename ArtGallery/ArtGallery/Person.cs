85ab8425e5f7fc866dd4424c84cf02221717c7e7
w3q4x <qweax98@gmail.com>
2020-03-19 14:16:23 +0900

    modified Person, addUsers

--- ArtGallery/ArtGallery/Person.cs
+++ ArtGallery/ArtGallery/Person.cs
@@ -13,7 +13,7 @@ namespace ArtGallery
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Syrname { get; set; }
+        public string Surname { get; set; }
         public string Patronymic { get; set; }
         public DateTime Birthday { get; set; }
     }
--- ArtGallery/ArtGallery/addUsers.cs
+++ ArtGallery/ArtGallery/addUsers.cs
@@ -15,6 +15,8 @@ namespace ArtGallery
         public addUsers()
         {
             InitializeComponent();
+            string[] access = System.Enum.GetNames(typeof(Access));
+            accessComboBox.Items.AddRange(access);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -25,6 +27,23 @@ namespace ArtGallery
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Employee employer = new Employee();
+            employer.Name = nameText.Text;
+            employer.Surname = SurnameText.Text;
+            employer.Patronymic = patronymicText.Text;
+            employer.Birthday = dateTime.Value;
+            employer.Access = (Access)accessComboBox.SelectedIndex;
+            employer.Login = loginText.Text;
+            employer.Password = passwordText.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
