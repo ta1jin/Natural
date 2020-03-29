8f99d0280708d26b99f1c1c7caf1cd8c1507a38f
BuildTools <unconfigured@null.spigotmc.org>
2020-03-30 00:36:55 +0900

    send to restoration update

--- ArtGallery/ArtGallery/SendToRestoration.Designer.cs
+++ ArtGallery/ArtGallery/SendToRestoration.Designer.cs
@@ -31,7 +31,10 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -39,7 +42,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1179, 316);
+            this.dataGridView1.Size = new System.Drawing.Size(667, 316);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -55,25 +58,47 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1031, 369);
+            this.button2.Location = new System.Drawing.Point(782, 369);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(160, 50);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Отправить на реставрацию выбранные картины";
+            this.button2.Text = "Пометить выбранные картины как нуждающиеся в реставрации";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(991, 369);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Отправить картины отмеченные как нуждающиеся в реставрации на реставрацию";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(685, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(518, 316);
+            this.dataGridView2.TabIndex = 4;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
             // SendToRestoration
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1215, 541);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "SendToRestoration";
             this.Text = "SendToRestoration";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -83,5 +108,7 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
--- ArtGallery/ArtGallery/SendToRestoration.cs
+++ ArtGallery/ArtGallery/SendToRestoration.cs
@@ -20,7 +20,7 @@ namespace ArtGallery
             RefreshList();
 
             DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
-            checkBoxColumn.HeaderText = "Отправить на реставрацию?";
+            checkBoxColumn.HeaderText = "Нуждается в реставрации?";
             checkBoxColumn.Name = "SendToRestoration";
             checkBoxColumn.ValueType = typeof(bool);
             checkBoxColumn.Width = 100;
@@ -38,6 +38,7 @@ namespace ArtGallery
             var paintings = from p in gc.Paintings
                             select p;
             DataTable dt = new DataTable();
+            DataTable dt2 = new DataTable();
 
 
 
@@ -51,10 +52,23 @@ namespace ArtGallery
             dt.Columns.Add("Price", typeof(double));
             dt.Columns.Add("State", typeof(state));
             dt.Columns.Add("Status", typeof(status));
+
+
+            dt2.Columns.Add("Id", typeof(int));
+            dt2.Columns.Add("Name", typeof(string));
+            dt2.Columns.Add("ArtistName", typeof(string));
+            dt2.Columns.Add("GenreName", typeof(string));
+            dt2.Columns.Add("PaintingTechniqueName", typeof(string));
+            dt2.Columns.Add("GalleryName", typeof(string));
+            dt2.Columns.Add("DateOfPainting", typeof(DateTime));
+            dt2.Columns.Add("Price", typeof(double));
+            dt2.Columns.Add("State", typeof(state));
+            dt2.Columns.Add("Status", typeof(status));
             foreach (Painting p in paintings)
             {
-                // NaRestavracii == 0 NaExposicii == 1 NaSklade == 2
-                if (p.Status != 0)
+                // Status: NaRestavracii == 0 NaExposicii == 1 NaSklade == 2
+                // State: Otlichnoe == 0 TrebuetRestavracii == 1 Util == 2 
+                if (p.Status == status.NaSklade)
                 {
                     var ArtistName = from a in gc.Artists
                                      where p.ArtistId == a.Id
@@ -86,12 +100,48 @@ namespace ArtGallery
                     drow["Status"] = p.Status;
                     dt.Rows.Add(drow);
                 }
+
+                if (p.State == state.TrebuetRestavracii)
+                {
+                    var ArtistName = from a in gc.Artists
+                                     where p.ArtistId == a.Id
+                                     select (a.Surname + " " + a.Name + " " + a.Patronymic).ToString();
+
+
+                    var GenreName = from g in gc.Genres
+                                    where p.GenreId == g.Id
+                                    select g.Name.ToString();
+
+                    var PaintingTechniqueName = from pT in gc.PaintingTechniques
+                                                where p.GenreId == pT.Id
+                                                select pT.Name.ToString();
+                    var GalleryName = from g in gc.Gallerys
+                                      where p.GenreId == g.Id
+                                      select g.Title
+                                      .ToString();
+                    string s = GenreName.First();
+                    DataRow drow;
+                    drow = dt2.NewRow();
+                    drow["Id"] = p.Id;
+                    drow["Name"] = p.Name;
+                    drow["ArtistName"] = ArtistName.First();
+                    drow["GenreName"] = s;
+                    drow["PaintingTechniqueName"] = PaintingTechniqueName.First();
+                    drow["GalleryName"] = GalleryName.First();
+                    drow["DateOfPainting"] = p.DateOfPainting;
+                    drow["Price"] = p.Price;
+                    drow["State"] = p.State;
+                    drow["Status"] = p.Status;
+                    dt2.Rows.Add(drow);
+                }
             }
 
 
 
             dataGridView1.DataSource = dt;
+            dataGridView2.DataSource = dt2;
             dataGridView1.Refresh();
+            dataGridView2.Refresh();
 
         }
 
@@ -118,16 +168,46 @@ namespace ArtGallery
                 // NaRestavracii == 0 NaExposicii == 1 NaSklade == 2
                 if (painting != null)
                 {
-                    painting.Status = 0;
+                    painting.State = state.TrebuetRestavracii;
                     gc.SaveChanges();
+                    //ShowExpositions.AddMovement
                 }
-                
+
             }
-            //ShowExpositions.AddMovement
+            
             RefreshList();
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<int> IDs = new List<int>();
+            foreach (DataGridViewRow dr in dataGridView1.Rows)
+            {
+                IDs.Add(Convert.ToInt32(dr.Cells["Id"].Value));
+            }
+            foreach (int i in IDs)
+            {
+                var painting = gc.Paintings.Where(p => p.Id == i).FirstOrDefault();
+                // NaRestavracii == 0 NaExposicii == 1 NaSklade == 2
+                if (painting != null)
+                {
+                    painting.Status = status.NaRestavracii;
+                    gc.SaveChanges();
+                    //ShowExpositions.AddMovement
+                    
+                }
+
+            }
+
+            RefreshList();
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
