42dc8e961376689840211f37dfdcfc23bfed4f70
nutunabe <baiytaan@gmail.com>
2020-05-07 20:38:13 +0900

    modified AddExposition

--- ArtGallery/ArtGallery/AddExposition.cs
+++ ArtGallery/ArtGallery/AddExposition.cs
@@ -19,23 +19,41 @@ namespace ArtGallery
 
         private void confirmDateBtn_Click(object sender, EventArgs e)
         {
-            using (GalleryContext gContext = new GalleryContext())
+            using (GalleryContext galleryContext = new GalleryContext())
             {
-                var allPaintings = gContext.Paintings.ToList();
-                if (gContext.Expositions.Any())
+
+                List<Painting> paintings = null;
+
+                var allPaintings = galleryContext.Paintings.ToList();
+
+                DataTable dataTable = new DataTable();
+                dataTable.Reset();
+
+                dataTable.Columns.Add("Id", typeof(int));
+                dataTable.Columns.Add("Name", typeof(string));
+                dataTable.Columns.Add("ArtistName", typeof(string));
+                dataTable.Columns.Add("GenreName", typeof(string));
+                dataTable.Columns.Add("PaintingTechniqueName", typeof(string));
+                dataTable.Columns.Add("GalleryName", typeof(string));
+                dataTable.Columns.Add("DateOfPainting", typeof(DateTime));
+                dataTable.Columns.Add("Price", typeof(double));
+                dataTable.Columns.Add("Status", typeof(status));
+                dataTable.Columns.Add("Condition", typeof(state));
+
+                if (galleryContext.Expositions.Any())
                 {
-                    var nap = gContext.Expositions.Where(expo =>
+                    var nonAvailPaintingsEnum = galleryContext.Expositions.Where(expo =>
                     (startDate.Value.CompareTo(expo.StartDate) > 0 && startDate.Value.CompareTo(expo.EndDate) < 0) ||
                     (endDate.Value.CompareTo(expo.StartDate) > 0 && endDate.Value.CompareTo(expo.EndDate) < 0))
                         .Select(p => p.Paintings)
                         .ToList();
 
                     List<Painting> nonAvailPaintings = new List<Painting>();
-                    for (int _i = 0; _i < nap.Count; _i++)
+                    for (int _i = 0; _i < nonAvailPaintingsEnum.Count; _i++)
                     {
-                        for (int _j = 0; _j < nap[_i].Count; _j++)
+                        for (int _j = 0; _j < nonAvailPaintingsEnum[_i].Count; _j++)
                         {
-                            nonAvailPaintings.Add(nap[_i].ToList()[_j]);
+                            nonAvailPaintings.Add(nonAvailPaintingsEnum[_i].ToList()[_j]);
                         }
                     }
 
@@ -49,17 +67,50 @@ namespace ArtGallery
                         }
                         if (_k == 0) availPaintings.Add(allPaintings[_i]);
                     }
-
-                    paintingsDataGridView.DataSource = availPaintings;
+                    paintings = availPaintings;
                 }
-                else paintingsDataGridView.DataSource = allPaintings;
+                if (paintings == null)
+                {
+                    var paints = from p in galleryContext.Paintings
+                                 select p;
+                    paintings = paints.ToList();
+                }
+                foreach (Painting p in paintings)
+                {
+                    var ArtistName = from a in galleryContext.Artists
+                                     where p.ArtistId == a.Id
+                                     select (a.Surname + " " + a.Name + " " + a.Patronymic).ToString();
 
-                paintingsDataGridView.Columns["ArtistId"].Visible = false;
-                paintingsDataGridView.Columns["GenreId"].Visible = false;
-                paintingsDataGridView.Columns["PaintingTehniqueId"].Visible = false;
-                paintingsDataGridView.Columns["GalleryId"].Visible = false;
-                paintingsDataGridView.Columns["Gallery"].Visible = false;
-                paintingsDataGridView.Columns["Reports"].Visible = false;
+                    var GenreName = from g in galleryContext.Genres
+                                    where p.GenreId == g.Id
+                                    select g.Name.ToString();
+
+                    var PaintingTechniqueName = from pT in galleryContext.PaintingTechniques
+                                                where p.GenreId == pT.Id
+                                                select pT.Name.ToString();
+
+                    var GalleryName = from g in galleryContext.Gallerys
+                                      where p.GalleryId == g.Id
+                                      select g.Title.ToString();
+
+                    string s = GenreName.First();
+                    DataRow dataRow;
+                    dataRow = dataTable.NewRow();
+                    dataRow["Id"] = p.Id;
+                    dataRow["Name"] = p.Name;
+                    dataRow["ArtistName"] = ArtistName.First();
+                    dataRow["GenreName"] = s;
+                    dataRow["PaintingTechniqueName"] = PaintingTechniqueName.First();
+                    dataRow["GalleryName"] = GalleryName.First();
+                    dataRow["DateOfPainting"] = p.DateOfPainting;
+                    dataRow["Price"] = p.Price;
+                    dataRow["Status"] = p.Status;
+                    dataRow["Condition"] = p.State;
+                    dataTable.Rows.Add(dataRow);
+                }
+                paintingsDataGridView.Refresh();
+                paintingsDataGridView.DataSource = dataTable;
+                paintingsDataGridView.Columns["Id"].Visible = false;
             }
         }
 
