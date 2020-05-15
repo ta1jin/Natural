cedab05660d9de29edb3e2a2e2196809d1c33b04
BuildTools <unconfigured@null.spigotmc.org>
2020-05-15 19:57:59 +0900

    update on show reports

--- ArtGallery/ArtGallery/ShowReports.Designer.cs
+++ ArtGallery/ArtGallery/ShowReports.Designer.cs
@@ -30,15 +30,17 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.BackButton = new System.Windows.Forms.Button();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(34, 23);
+            this.dataGridView1.Location = new System.Drawing.Point(492, 23);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1062, 445);
+            this.dataGridView1.Size = new System.Drawing.Size(604, 445);
             this.dataGridView1.TabIndex = 13;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -52,17 +54,38 @@
             this.BackButton.UseVisualStyleBackColor = true;
             this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
             // 
+            // DescriptionTextBox
+            // 
+            this.DescriptionTextBox.Location = new System.Drawing.Point(12, 77);
+            this.DescriptionTextBox.Multiline = true;
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            this.DescriptionTextBox.Size = new System.Drawing.Size(464, 391);
+            this.DescriptionTextBox.TabIndex = 14;
+            this.DescriptionTextBox.TextChanged += new System.EventHandler(this.DescriptionTextBox_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(464, 34);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
             // ShowReports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1134, 589);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.DescriptionTextBox);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.BackButton);
             this.Name = "ShowReports";
             this.Text = "ShowReports";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,5 +93,7 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- ArtGallery/ArtGallery/ShowReports.cs
+++ ArtGallery/ArtGallery/ShowReports.cs
@@ -13,10 +13,20 @@ namespace ArtGallery
     public partial class ShowReports : Form
     {
         GalleryContext gc = new GalleryContext();
+        string DescriptionColumnName = "Description";
+        string TitleColumnName = "Title"; 
         public ShowReports()
         {
             InitializeComponent();
             RefreshList();
+
+            DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
+            checkBoxColumn.HeaderText = "Посмотреть содержимое отчета подробнее";
+            checkBoxColumn.Name = "CheckReport";
+            checkBoxColumn.ValueType = typeof(bool);
+            checkBoxColumn.Width = 100;
+            dataGridView1.Columns.Add(checkBoxColumn);
+            dataGridView1.Columns["CheckReport"].DisplayIndex = 0;
         }
 
         private void BackButton_Click(object sender, EventArgs e)
@@ -26,7 +36,7 @@ namespace ArtGallery
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            
         }
 
         public void RefreshList()
@@ -56,8 +66,8 @@ namespace ArtGallery
                 DataRow drow;
                 drow = dt.NewRow();
                 drow["Id"] = r.Id;
-                drow["Title"] = r.Title;
-                drow["Description"] = r.Description;
+                drow[TitleColumnName] = r.Title;
+                drow[DescriptionColumnName] = r.Description;
                 drow["ReportDate"] = r.ReportDate;
                 drow["EmployeeId"] = r.EmployeeId;
                 //TODO
@@ -73,6 +83,53 @@ namespace ArtGallery
 
             dataGridView1.DataSource = dt;
             dataGridView1.Refresh();
+
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            int columnIndex = 0;
+            if (e.ColumnIndex == columnIndex)
+            {
+                bool isChecked = (bool)dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value;
+                if (isChecked)
+                {
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        if (row.Index != e.RowIndex)
+                        {
+                            row.Cells[columnIndex].Value = !isChecked;
+                        }
+                        else
+                        {
+                            for (int i = 0;i< dataGridView1.Columns.Count; i++)
+                            {
+                                if (dataGridView1.Columns[i].Name == DescriptionColumnName)
+                                {
+                                    DescriptionTextBox.Text = row.Cells[i].Value.ToString();
+                                }
+                                else if (dataGridView1.Columns[i].Name == TitleColumnName)
+                                {
+                                    textBox1.Text = row.Cells[i].Value.ToString();
+                                }
+                            }
+                            
+                            
+                        }
+                    }
+                }
+            }
+
+            
+        }
+
+        private void DescriptionTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
