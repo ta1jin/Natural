4d8da35545fdb8537db723c89597aa3968505ed5
nutunabe <baiytaan@gmail.com>
2020-05-28 10:40:01 +0900

    modified StatusChecker

---
+++ ArtGallery/ArtGallery/StatusChecker.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtGallery
+{
+    class StatusChecker
+    {
+        public static void AutoCheck()
+        {
+            // . . .
+        }
+
+        public static void CheckPaintingsForStatus()
+        {
+            using (GalleryContext galleryContext = new GalleryContext())
+            {
+                if (galleryContext.Paintings.Any())
+                {
+                    List<Painting> paintings = galleryContext.Paintings.ToList();
+                    foreach (Painting painting in paintings)
+                    {
+                        galleryContext.Entry(painting).Collection(p => p.Expositions).Load();
+                        bool isOnExpo = false;
+                        foreach (Exposition exposition in painting.Expositions)
+                        {
+                            if (exposition.Status != ExpositionStatus.Cancelled &&
+                                (DateTime.Now.CompareTo(exposition.StartDate.Subtract(new TimeSpan(0, 1, 0))) >= 0
+                                && DateTime.Now.CompareTo(exposition.EndDate.Subtract(new TimeSpan(0, 1, 0))) <= 0))
+                            {
+                                isOnExpo = true;
+                            }
+                        }
+                        if (isOnExpo)
+                        {
+                            painting.Status = status.NaExposicii;
+                        }
+                        else
+                        {
+                            painting.Status = status.NaSklade;
+                        }
+                        galleryContext.Entry(painting).State = System.Data.Entity.EntityState.Modified;
+                    }
+
+                    galleryContext.SaveChanges();
+                }
+            }
+        }
+
+        public static void CheckExpositionsForStatus()
+        {
+            using (GalleryContext galleryContext = new GalleryContext())
+            {
+                if (galleryContext.Expositions.Any())
+                {
+                    List<Exposition> expositions = galleryContext.Expositions.ToList();
+                    foreach (Exposition exposition in expositions)
+                    {
+                        if (exposition.Status != ExpositionStatus.Cancelled)
+                        {
+                            if (DateTime.Now.CompareTo(exposition.StartDate.Subtract(new TimeSpan(0, 1, 0))) >= 0
+                                && DateTime.Now.CompareTo(exposition.EndDate.Subtract(new TimeSpan(0, 1, 0))) <= 0)
+                            {
+                                exposition.Status = ExpositionStatus.Active;
+                            }
+                            else if (DateTime.Now.CompareTo(exposition.StartDate.Subtract(new TimeSpan(0, 1, 0))) < 0)
+                            {
+                                exposition.Status = ExpositionStatus.Scheduled;
+                            }
+                            else if (DateTime.Now.CompareTo(exposition.StartDate.Subtract(new TimeSpan(0, 1, 0))) > 0)
+                            {
+                                exposition.Status = ExpositionStatus.Cancelled;
+                            }
+                            galleryContext.Entry(exposition).State = System.Data.Entity.EntityState.Modified;
+                        }
+                    }
+
+                    galleryContext.SaveChanges();
+                }
+            }
+        }
+    }
+}
