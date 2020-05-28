5d1dc59f390e33b19a9c068df1a3fb22891ca855
nutunabe <baiytaan@gmail.com>
2020-05-28 12:05:16 +0900

    poryadok

---
+++ ArtGallery/ArtGallery/Models/Exposition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ArtGallery
+{
+    public enum ExpositionStatus
+    {
+        Scheduled,
+        Active,
+        Cancelled
+    }
+    public class Exposition
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ExpositionStatus Status { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int ShowroomId { get; set; }
+        [ForeignKey("ShowroomId")]
+        public virtual Showroom Showroom { get; set; }
+        public int GalleryId { get; set; }
+        [ForeignKey("GalleryId")]
+        public virtual Gallery Gallery { get; set; }
+        public ICollection<Painting> Paintings { get; set; }
+    }
+}
